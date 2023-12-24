07a82065d7c7b144f28ddde75081e3f6c44eeaee
Khanh Le <masterbruculous@gmail.com>
2023-12-24 19:04:41 +0700

    Create a view to represent a UIPageViewController

---
+++ Landmarks/View/PageView.swift
@@ -0,0 +1,22 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by MasterBi on 24/12/2023.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    var body: some View {
+        PageViewController(pages: pages)
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
+
+#Preview {
+    PageView(pages: ModelData().features.map {
+        FeatureCard(landmark: $0)
+    })
+}
