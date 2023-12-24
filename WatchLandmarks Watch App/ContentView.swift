ede44de4a7b8b79badeadc236e72fdca3d558157
Khanh Le <masterbruculous@gmail.com>
2023-12-24 20:44:24 +0700

    Added watchOS app

---
+++ WatchLandmarks Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WatchLandmarks Watch App
+//
+//  Created by MasterBi on 24/12/2023.
+//
+
+import SwiftUI
+import UserNotifications
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .task {
+                let center = UNUserNotificationCenter.current()
+                _ = try? await center.requestAuthorization(options: [.alert, .sound, .badge])
+            }
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
